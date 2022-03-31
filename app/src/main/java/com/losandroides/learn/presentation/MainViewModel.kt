44afa70b9bcf3b1d603891bc91c90a16a84cf18e
Joan <joancolmenero@gmail.com>
2022-03-31 11:45:37 +0200

    Inject coroutine dispatcher instead of hardcoding

--- app/src/main/java/com/losandroides/learn/presentation/MainViewModel.kt
+++ app/src/main/java/com/losandroides/learn/presentation/MainViewModel.kt
@@ -2,10 +2,11 @@ package com.losandroides.learn.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.losandroides.learn.di.IO
 import com.losandroides.learn.domain.ItemsUseCase
 import com.losandroides.learn.domain.model.Item
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -13,7 +14,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val itemsUseCase: ItemsUseCase
+    private val itemsUseCase: ItemsUseCase,
+    @IO private val dispatcher: CoroutineDispatcher,
 ) : ViewModel() {
 
     init {
@@ -24,7 +26,7 @@ class MainViewModel @Inject constructor(
     val viewState: StateFlow<ViewState> = _viewState
 
     private fun getItems() {
-        viewModelScope.launch(Dispatchers.IO) {
+        viewModelScope.launch(dispatcher) {
             val items = itemsUseCase()
             _viewState.value = ViewState.Content(items)
         }
--- app/src/test/java/com/losandroides/learn/presentation/MainViewModelTest.kt
+++ app/src/test/java/com/losandroides/learn/presentation/MainViewModelTest.kt
@@ -1,57 +1,39 @@
 package com.losandroides.learn.presentation
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.losandroides.learn.domain.ItemsUseCase
 import com.losandroides.learn.domain.model.Item
 import com.losandroides.learn.framework.coVerifyOnce
-import com.losandroides.learn.framework.relaxedMockk
 import io.mockk.coEvery
-import kotlinx.coroutines.Dispatchers
+import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.*
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
 import org.amshove.kluent.shouldBeEqualTo
-import org.junit.After
-import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 
 @ExperimentalCoroutinesApi
 class MainViewModelTest {
 
-    @get:Rule
-    val instantTaskExecutorRule = InstantTaskExecutorRule()
-
     private val testDispatcher = StandardTestDispatcher()
-    private val testCoroutineScope = TestScope(testDispatcher)
-
-    private val itemsUseCase: ItemsUseCase = relaxedMockk()
-
-    @Before
-    fun setUp() {
-        Dispatchers.setMain(testDispatcher)
-    }
-
-    @After
-    fun cleanup() {
-        Dispatchers.resetMain()
-    }
+    private val itemsUseCase = mockk<ItemsUseCase>()
 
     @Test
-    fun `GIVEN some items WHEN MainViewModel is initialized THEN ItemsUseCase is called`() {
-        val items = buildItems()
-        coEvery { itemsUseCase() } returns items
+    fun `GIVEN some items WHEN MainViewModel is initialized THEN ItemsUseCase is called`() =
+        runTest(testDispatcher) {
+            val items = buildItems()
+            coEvery { itemsUseCase() } returns items
 
-        testCoroutineScope.runTest {
             val viewModel = buildMainViewModel()
+            runCurrent()
 
             coVerifyOnce { itemsUseCase() }
-
             viewModel.viewState.value shouldBeEqualTo MainViewModel.ViewState.Content(items)
         }
-    }
 
     private fun buildMainViewModel() = MainViewModel(
-        itemsUseCase
+        itemsUseCase = itemsUseCase,
+        dispatcher = testDispatcher,
     )
 
     private fun buildItems() = listOf(
