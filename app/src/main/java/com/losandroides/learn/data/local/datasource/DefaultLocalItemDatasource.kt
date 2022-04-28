34c990e43b6818eda7c4f7d53a4b40b5a40cc60c
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2022-04-28 09:25:08 +0200

    Remove dispatcher in DefaultLocalItemDatasource

--- app/src/main/java/com/losandroides/learn/data/local/datasource/DefaultLocalItemDatasource.kt
+++ app/src/main/java/com/losandroides/learn/data/local/datasource/DefaultLocalItemDatasource.kt
@@ -4,34 +4,24 @@ import arrow.core.Either
 import com.losandroides.learn.data.local.database.ItemDatabase
 import com.losandroides.learn.data.toDatabaseModel
 import com.losandroides.learn.data.toDomain
-import com.losandroides.learn.di.IO
 import com.losandroides.learn.domain.model.Item
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class DefaultLocalItemDatasource @Inject constructor(
-    database: ItemDatabase,
-    @IO private val dispatcher: CoroutineDispatcher,
-) : LocalItemDatasource {
+class DefaultLocalItemDatasource @Inject constructor(database: ItemDatabase) : LocalItemDatasource {
 
     private val dao = database.itemDatabaseModelDao()
 
     override suspend fun isEmpty(): Boolean =
-        withContext(dispatcher) { dao.itemsCount() <= 0 }
+        dao.itemsCount() <= 0
 
     override suspend fun saveItems(items: List<Item>) {
-        withContext(dispatcher) {
-            dao.insertItems(items.map {
-                it.toDatabaseModel()
-            })
-        }
+        dao.insertItems(items.map {
+            it.toDatabaseModel()
+        })
     }
 
     override suspend fun getAllItems(): Either<Throwable, List<Item>> =
-        withContext(dispatcher) {
-            Either.catch {
-                dao.getAllItems().toDomain()
-            }
+        Either.catch {
+            dao.getAllItems().toDomain()
         }
 }
